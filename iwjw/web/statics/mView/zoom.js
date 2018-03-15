f10efe754844c9469ce5457ad11ddbcdd3e0b7c9
GitDesktopOptions <1315010331@qq.com>
2018-03-15 23:50:29 +0800

    修复font-awesome矢量图不显示的问题

--- iwjw/web/statics/mView/zoom.js
+++ iwjw/web/statics/mView/zoom.js
@@ -100,7 +100,6 @@ ComplexCustomOverlay.prototype.initialize = function (map) {
     };
     this.getJS = function () {
         //通过jquery获取js
-        // $.getScript('../baiduMap/mView/zoom1.js').done(function () {
         obj.json3.data.markList.forEach(element => {
             var point1 = new BMap.Point(element.lon, element.lat);
             //创建覆盖物对象，参数1 为经纬度，2 为文本，3 为鼠标移上去的样式
@@ -110,10 +109,6 @@ ComplexCustomOverlay.prototype.initialize = function (map) {
             //将覆盖物添加到地图
             map.addOverlay(myCompOverlays);
         });
-
-        // }).fail(function (jqxhr, settings, exception) {
-        //     // console.log("======error========"+jqxhr+" : "+settings+" : "+exception);
-        // });
     }
     map.getPanes().labelPane.appendChild(div);
     return div;
@@ -216,7 +211,6 @@ ComplexCustomOverlay_small.prototype.initialize = function (map) {
     };
     this.getJS = function () {
         //通过jquery获取js
-        // $.getScript('../baiduMap/mView/zoom2.js').done(function () {
         var i = 0;
         obj.json4.data.markList.forEach(element => {
             i++;
@@ -231,9 +225,6 @@ ComplexCustomOverlay_small.prototype.initialize = function (map) {
             map.addOverlay(myCompOverlays1);
         });
 
-        // }).fail(function (jqxhr, settings, exception) {
-        //     // console.log("======error========"+jqxhr+" : "+settings+" : "+exception);
-        // });
     }
     map.getPanes().labelPane.appendChild(div);
     return div;
@@ -253,7 +244,6 @@ ComplexCustomOverlay_small.prototype.draw = function () {
 //====================================三级点=========================================
 
 // 自定义覆盖物
-// function ComplexCustomOverlay_s_small(point, text, sub_text, boundary, obj-bf) {
 function ComplexCustomOverlay_s_small(point, text, sub_text) {
     this._point = point;
     this._text = text;
